836915a1994a7bf0ea22bb71159f522fb4bffb11
brbiancr <brbiancr@gmail.com>
2024-02-23 14:25:19 -0300

    Corrigindo cabecalho da funcao fitness()

--- fitness.h
+++ fitness.h
@@ -1,4 +1,4 @@
 #include "main.h"
 
-void fitness(int **tabuleiro, int *fitnessDaPopulacao);
+void fitness(int **tabuleiro, int *fitnessDaPopulacao, int encontrouSolucao);
 void posicionaRainhas(int individuo, int **tabuleiro);
