67845e5e5374d8d5e95152e2cfb59a651d0ccdd7
brbiancr <brbiancr@gmail.com>
2024-03-13 16:56:31 -0300

    Transfomando fitnessDaPopulacao e populacaoAtual para variaveis locais

--- include/main.h
+++ include/main.h
@@ -1,12 +1,9 @@
 // Defines
 #define TAMANHOPOPULACAO 1000
-#define TAMANHOTABULEIRO 16
+#define TAMANHOTABULEIRO 13
 #define QUANTIDADEINDIVIDUOSPORTORNEIO 5
 #define TAXAELITISMO 0.2
 #define TAXAMUTACAO 0.08
-#define TIPODESELECAO 4 // 1- Aleatoria. 2- Roleta. 3- Torneio. 4- Torneio dos dissimilares
+#define TIPODESELECAO 3 // 1- Aleatoria. 2- Roleta. 3- Torneio. 4- Torneio dos dissimilares
 #define TIPODECRUZAMENTO 2 // 1- Cruzamento de um ponto. 2- Cruzamento de dois pontos
 
-// Vari�veis globais
-int **populacaoAtual;
-int *fitnessDaPopulacao;
